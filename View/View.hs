e07dc3dfddc39ad685abeb9a2b567b6b8de4e862
Chris Done <chrisdone@gmail.com>
2008-05-23 23:37:42 +0100

    rewrote View.hs: handles Wikipedia download error gracefully, caches image URLs, generally nicer looking code

--- View/View.hs
+++ View/View.hs
@@ -11,17 +11,66 @@ import Text.XML.HaXml.Xtract.Parse
 import Data.Maybe
 import Data.List
 import Text.XHtml.Strict
+import IO
 
-type DB = [(String,String,String)]
+type Entry = (String,String,String)
+type DB = [Entry]
 
 view :: CGI CGIResult
 view = output . showHtml =<< do
-  db <- liftIO $ gismuDB
-  (img,gismu) <- randImage db
+  db <- getDB
+  rEntry <- randomEntry db
+  case rEntry of
+    Just entry -> showEntry entry
+    Nothing    -> retFail db
+
+showEntry :: Entry -> CGI Html
+showEntry (_,gismu,url) = do
+  return $ h1 << gismu
+           +++
+           image ! [src url]
+
+retFail :: DB -> CGI Html
+retFail db = do
+  tryEx <- doExistingEntry db
+  return $ p << "Couldn't retrieve entry from Wikipedia."
+           +++
+           tryEx
+
+doExistingEntry :: DB -> CGI Html
+doExistingEntry db = do
+  rExEntry <- randomEntry $ filter hasURL db
+  case rExEntry of
+    Just entry -> showEntry entry
+    Nothing    -> return $ p << "Could not get any cached entries either!"
+    where hasURL (_,_,"") = False
+          hasURL _        = True
+
+randomEntry :: DB -> CGI (Maybe Entry)
+randomEntry [] = return Nothing
+randomEntry db = do
+  i <- randomIndex db
+  case db !! i of
+    e@(img,gismu,"") -> tryLookup db e
+    entry            -> return $ Just entry
+
+tryLookup :: DB -> Entry -> CGI (Maybe Entry)
+tryLookup db (img,gismu,_) = do
   url <- imageLookup img
   case url of
-    Just url -> return $ h1 << gismu +++ p << image ! [src url]
-    Nothing  -> return $ p << ("Couldn't get image address for " ++ img)
+    Just url' -> do updateDBURL db img url'
+                    return $ Just (img,gismu,url')
+    _         -> return $ Nothing
+
+updateDBURL :: DB -> String -> String -> CGI ()
+updateDBURL db img url = liftIO $ do
+  let newDB = mapMaybe update db
+      update (img',gismu,_) | img' == img = Just (img,gismu,url)
+      update (img,gismu,u) = Just (img,gismu,u)
+  h <- openFile "gismu.db" WriteMode
+  hSetBuffering h NoBuffering
+  hPutStr h $ show newDB
+  hClose h
 
 imageLookup :: String -> CGI (Maybe String)
 imageLookup img = do
@@ -30,11 +79,8 @@ imageLookup img = do
      then return $ getUrl body
      else return Nothing
   where url = "http://commons.wikimedia.org/w/api.php?action=query&titles="
-              ++ img ++
+              ++ urlEncode(img) ++
               "&prop=imageinfo&iiprop=url&&iiurlwidth=500&format=xml"
-
-repSpaces :: String -> String
-repSpaces = id
 
 getUrl :: String -> Maybe String
 getUrl xml =
@@ -49,15 +95,10 @@ innerUrl elem =
 
 extract xml = xtract "//ii[@thumburl]" cont where
     cont = CElem root    
-    (Document _ _ root _) = xmlParse "Wikipedia API" xml    
+    (Document _ _ root _) = xmlParse "Wikipedia API" xml  
 
-randImage :: DB -> CGI (String,String)
-randImage db = liftIO $ do
-  r <- getStdRandom $ randomR (0, length db - 1)
-  let (img,gismu,_) = db !! r
-  return $ (urlEncode img,gismu)
+randomIndex :: DB -> CGI Int
+randomIndex db = liftIO $ getStdRandom $ randomR (0,length db-1)
 
-gismuDB :: IO DB
-gismuDB = do
-  db <- readFile "gismu.db"
-  return $ read db :: IO DB
+getDB :: CGI DB
+getDB = liftIO $ liftM read $ readFile "gismu.db"
