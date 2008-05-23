f2bae05160d19800f43ef8b858f9fc1ea090796c
Chris Done <chrisdone@gmail.com>
2008-05-23 13:49:49 +0100

    fixed parsing of HTML entites by using the shibby TagSoup library

--- Update/Update.hs
+++ Update/Update.hs
@@ -4,18 +4,30 @@ import Network.Curl
 import Text.Regex
 import Data.Maybe
 import IO
+import Text.HTML.TagSoup
+import Data.List
 
 update :: IO ()
 update = do
   (code,body) <- curlGetString url []
   if code == CurlOK 
-     then do
-       h <- openFile "gismu.db" WriteMode
-       hPutStr h $ show $ gismuList body
-       hFlush h
+     then case entry $ parseTags body of
+            Just entry' -> do
+              h <- openFile "gismu.db" WriteMode
+              hPutStr h $ show $ gismus entry'
+              hFlush h
+            _           -> return ()
      else return ()
-    where gismuList = map pair . mapMaybe (matchRegex regex) . lines
+    where gismus = map pair . mapMaybe (matchRegex regex) . lines
           pair [a,b] = (a,b,"") -- Intentional empty image URL entry
           regex = mkRegex "^(Image:.*)\\|\\[\\[(.*)\\]\\]$"
           url = "http://jbo.wikipedia.org/w/index.php?\
                 \title=pixra_liste_loi_gismu&action=edit"
+
+entry tags = text where
+    text = case ts of
+             Just (_:TagText t:_) -> Just t
+             _                    -> Nothing
+    ts = find textarea $ tails tags
+    textarea ((TagOpen "textarea" _):(TagText _):_) = True
+    textarea _ = False
